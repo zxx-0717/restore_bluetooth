71ed1d03ea8ce615939b05d2e1ac1ea1074b7079
zxx-0717 <1024988917@qq.com>
2024-04-12 16:33:46 +0800

    add reboot robot function in restore_bluetooth_test.sh

--- restore_bluetooth_test.sh
+++ restore_bluetooth_test.sh
@@ -8,15 +8,17 @@ echo ""
 
 bluetooth_controller="tj2022-DEFAULT-STRING"
 charger_bluetooth_name="ai-thinker2"
-time_interval=5
+time_interval=60
 
 restart_core_num_max=2
 restart_core_num=0
 reboot_robot_num_max=2
 reboot_robot_num=$(cat /opt/systemd-sh/reboot_robot_num.txt)
+reboot_robot_num_last=$reboot_robot_num
 
 while true;
 do
+    echo '------------------------------------------------------'
     if cat /home/tj2022/map/core_restart.txt | grep -q 1 ; then 
         if hciconfig -a | grep -iq $bluetooth_controller;then
             if bluetoothctl devices | grep -iq $charger_bluetooth_name;then
@@ -24,6 +26,8 @@ do
                 echo ${str_output} | tee -a $log_file_name
                 restart_core_num=0
                 reboot_robot_num=0
+                reboot_robot_num_last=0
+                echo 'tj2022' | sudo -S echo $reboot_robot_num > /opt/systemd-sh/reboot_robot_num.txt
             else
                 str_output=$(date)" === Bluetooth malfunction detected: : cann't find ${charger_bluetooth_name} bluetooth"
                 echo ${str_output} | tee -a $log_file_name
@@ -38,31 +42,35 @@ do
         echo $(date)" --- not in monitoring state." | tee -a $log_file_name
         restart_core_num=0
         reboot_robot_num=0
+        reboot_robot_num_last=0
+        echo 'tj2022' | sudo -S echo $reboot_robot_num > /opt/systemd-sh/reboot_robot_num.txt
     fi
 
     if [ $restart_core_num -gt 0 ]; then
         if [ $restart_core_num -le $restart_core_num_max ]; then
-            # docker compose -f /home/tj2022/docker-compose.yml restart core
-            str_output=$(date)" === fake restart core "
+            docker compose -f /home/tj2022/docker-compose.yml restart core
+            str_output=$(date)" ===  restart core "
             echo ${str_output} | tee -a $log_file_name
         else
-            reboot_robot_num=$(($reboot_robot_num + 1))
+            if [ $reboot_robot_num -le $reboot_robot_num_max ]; then
+                reboot_robot_num=$(($reboot_robot_num + 1))
+            fi
         fi
     fi
 
-    if [[ $reboot_robot_num -gt 0 && $restart_core_num -ge $restart_core_num_max ]]; then
-        if [ $reboot_robot_num -lt $reboot_robot_num_max ]; then
+    if [ $reboot_robot_num -ne $reboot_robot_num_last ]; then
+        if [ $reboot_robot_num -le $reboot_robot_num_max ]; then
             restart_core_num=0
             echo 'tj2022' | sudo -S echo $reboot_robot_num > /opt/systemd-sh/reboot_robot_num.txt
             str_output=$(date)" === reboot robot "${reboot_robot_num}" time"
             echo ${str_output} | tee -a $log_file_name
-            # reboot
+            reboot_robot_num_last=$reboot_robot_num
+            reboot
         else
             str_output=$(date)" === reboot robot exceeded max number, just do nothing"
             echo $str_output | tee -a $log_file_name
         fi
     fi
-
 
     sleep $time_interval
 done
