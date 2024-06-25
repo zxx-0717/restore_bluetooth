2c6592e6ed0f7f2e0d7f919c0674fbfa70154557
zxx-0717 <1024988917@qq.com>
2024-06-25 17:05:44 +0800

    add cmd for restore bluetooth

--- restore_bluetooth.sh
+++ restore_bluetooth.sh
@@ -9,6 +9,7 @@ echo ""
 bluetooth_controller="tj2022-DEFAULT-STRING"
 charger_bluetooth_name="ai-thinker"
 time_interval=60
+pwd="tj2022"
 
 while true;
 do
@@ -18,13 +19,21 @@ do
                 str_output=$(date)" === normal"
                 echo ${str_output} | tee -a $log_file_name
             else
-                str_output=$(date)" === restart core for reason: cann't find ${charger_bluetooth_name} bluetooth"
+                str_output=$(date)" === restore bluetooth for reason: cann't find ${charger_bluetooth_name} bluetooth"
                 echo ${str_output} | tee -a $log_file_name
                 docker compose -f /home/tj2022/docker-compose.yml restart core
             fi
         else
-            str_output=$(date)" === restart core for reason: donn't have ${bluetooth_controller} controller"
+            str_output=$(date)" === restore bluetooth for reason: donn't have ${bluetooth_controller} controller"
             echo ${str_output} | tee -a $log_file_name
+            echo $pwd | sudo -S systemctl restart bluetooth
+            echo $pwd | sudo -S hciconfig hci0 down
+            echo $pwd | sudo -S hciconfig hci0 up
+            echo $pwd | sudo -S rfkill unblock all
+            echo $pwd | sudo -S rmmod btusb
+            echo $pwd | sudo -S rmmod btintel
+            echo $pwd | sudo -S modprobe btintel
+            echo $pwd | sudo -S modprobe btusb
             docker compose -f /home/tj2022/docker-compose.yml restart core
         fi
     else
