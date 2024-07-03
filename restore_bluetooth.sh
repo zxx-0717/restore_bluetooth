613ddc91fcbcf36a8061c2d4201d8bc22860b141
zxx-0717 <1024988917@qq.com>
2024-07-03 14:15:46 +0800

    add branch optimize

--- restore_bluetooth.sh
+++ restore_bluetooth.sh
@@ -8,41 +8,52 @@ echo ""
 
 bluetooth_controller="tj2022-DEFAULT-STRING"
 charger_bluetooth_name="ai-thinker"
-time_interval=60
+time_interval=10
 pwd="tj2022"
 
 while true;
 do
     if cat /home/tj2022/map/core_restart.txt | grep -q 1 ; then 
-        if hciconfig -a | grep -iq $bluetooth_controller;then
-            if bluetoothctl devices | grep -iq $charger_bluetooth_name;then
-                str_output=$(date)" === normal"
-                echo ${str_output} | tee -a $log_file_name
-            else
-                str_output=$(date)" === restore bluetooth for reason: cann't find ${charger_bluetooth_name} bluetooth"
-                echo ${str_output} | tee -a $log_file_name
-                echo $pwd | sudo -S systemctl restart bluetooth
-                echo $pwd | sudo -S hciconfig hci0 down
-                echo $pwd | sudo -S hciconfig hci0 up
-                echo $pwd | sudo -S rfkill unblock all
-                echo $pwd | sudo -S rmmod btusb
-                echo $pwd | sudo -S rmmod btintel
-                echo $pwd | sudo -S modprobe btintel
-                echo $pwd | sudo -S modprobe btusb
-                docker compose -f /home/tj2022/docker-compose.yml restart core
-            fi
-        else
-            str_output=$(date)" === restore bluetooth for reason: donn't have ${bluetooth_controller} controller"
+        if cat /home/tj2022/map/bluetooth_restore.txt | grep -q 1 ;then
+
+            str_output=$(date)" === restoring bluetooth ...... "
             echo ${str_output} | tee -a $log_file_name
-            echo $pwd | sudo -S systemctl restart bluetooth
-            echo $pwd | sudo -S hciconfig hci0 down
-            echo $pwd | sudo -S hciconfig hci0 up
-            echo $pwd | sudo -S rfkill unblock all
-            echo $pwd | sudo -S rmmod btusb
-            echo $pwd | sudo -S rmmod btintel
-            echo $pwd | sudo -S modprobe btintel
-            echo $pwd | sudo -S modprobe btusb
+
+            str_output=$(date)" === exec systemctl restart bluetooth"
+            echo ${str_output} | tee -a $log_file_name
+            timeout 10 echo $pwd | sudo -S systemctl restart bluetooth
+
+            str_output=$(date)" === exec hciconfig hci0 down"
+            echo ${str_output} | tee -a $log_file_name
+            timeout 5 echo $pwd | sudo -S hciconfig hci0 down
+
+            str_output=$(date)" === exec hciconfig hci0 up"
+            echo ${str_output} | tee -a $log_file_name
+            timeout 5 echo $pwd | sudo -S hciconfig hci0 up
+
+            str_output=$(date)" === exec rfkill unblock all"
+            echo ${str_output} | tee -a $log_file_name
+            timeout 5 echo $pwd | sudo -S rfkill unblock all
+
+            str_output=$(date)" === exec rmmod btusb"
+            echo ${str_output} | tee -a $log_file_name
+            timeout 5 echo $pwd | sudo -S rmmod btusb
+
+            str_output=$(date)" === exec rmmod btintel"
+            echo ${str_output} | tee -a $log_file_name
+            timeout 5 echo $pwd | sudo -S rmmod btintel
+
+            str_output=$(date)" === exec modprobe btintel"
+            echo ${str_output} | tee -a $log_file_name
+            timeout 5 echo $pwd | sudo -S modprobe btintel
+
+            str_output=$(date)" === exec modprobe btusb"
+            echo ${str_output} | tee -a $log_file_name
+            timeout 5 echo $pwd | sudo -S modprobe btusb
             docker compose -f /home/tj2022/docker-compose.yml restart core
+        else
+            str_output=$(date)" === normal"
+            echo ${str_output} | tee -a $log_file_name
         fi
     else
         echo $(date)" --- not in monitoring state." | tee -a $log_file_name
